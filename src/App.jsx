1f6c3085ea811f45dc60124f222e66ed8bb5e26f
Nicolas Ibañez <nicowxdvd@gmail.com>
2026-09-15 12:32:28 -0300

    Proyecto base

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  return <div>Lumina</div>
+}
+
+
+
+export default App
